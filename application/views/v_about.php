d9c13a2aabca129c4f07b085bc17c64f6be27ab1
heryantokawisen <heryantokawisen2003@gmail.com>
2024-04-22 16:13:24 +0700

    Pertemuan 4 dan Tugas  3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+  <h1><?php echo $judul ?></h1>
+  <h4>Nama</h4>
+  <ul type="disc">
+     <li>Nama Depan : Heryanto</li>
+     <li>Nama Belakang : Kawisen</li>
+  </ul>
+  <br>
+  <h4>Alamat</h4>
+  <ul type="none">
+    <li> Jalan Pangsuma RT 1 /RW 1 Putussibau Selatan</li>
+  </ul>
+ 
+  <h4>Tempat Lahir</h4>
+  <ul type="Tamao">
+  <li>Kabupaten kapuas hulu</li>
+  </ul>
+  <h4>Olah Raga Favorit</h4>
+  <ul type="square">
+     <li>Bulutangkis</li>
+    <li></li>Tenis Meja
+  </ul>
+</section>
