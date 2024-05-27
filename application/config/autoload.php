319f801dde634d9b316bf68ae1749e2553ed1319
heryantokawisen <heryantokawisen2003@gmail.com>
2024-05-27 16:50:50 +0700

    Pertemuan 10 dan tugas 8

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array('');
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url', 'file', 'pustaka');
 
 /*
 | -------------------------------------------------------------------
@@ -133,6 +133,6 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('ModelUser', 'ModelBuku');
 
 
